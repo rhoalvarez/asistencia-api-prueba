c97bd90df60732691ebc282e047859c3faa3a23e
rhoalvarez <alvarezrho@gmail.com>
2026-09-23 22:59:43 -0300

    Permitir conexion desde verce

--- attendance_api/app/main.py
+++ attendance_api/app/main.py
@@ -25,7 +25,8 @@ app.add_middleware(
         "http://127.0.0.1:5173",
         "http://127.0.0.1:5174",
         "https://rhoalvarez.github.io",
-        "https://ies-administrativo-prueba.vercel.app/",
+
+        "https://ies-administrativo-prueba.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
