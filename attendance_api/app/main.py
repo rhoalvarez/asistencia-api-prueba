a4a9af8ae4b843b5efe2775ed60247e98d4250d6
rhoalvarez <alvarezrho@gmail.com>
2026-09-23 22:43:17 -0300

    Subir a vercel

--- attendance_api/app/main.py
+++ attendance_api/app/main.py
@@ -25,6 +25,7 @@ app.add_middleware(
         "http://127.0.0.1:5173",
         "http://127.0.0.1:5174",
         "https://rhoalvarez.github.io",
+        "https://ies-administrativo-prueba.vercel.app/",
     ],
     allow_credentials=True,
     allow_methods=["*"],
